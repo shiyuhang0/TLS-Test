c94584a3dd3b53dddab9701c8cdba42a2d881e34
shiyuhang <1136742008@qq.com>
2022-12-02 22:01:15 +0800

    Test java2

--- example_code/java/TLS_JAVA.java
+++ example_code/java/TLS_JAVA.java
@@ -9,13 +9,21 @@ public class TLS_JAVA {
     String host = args[1];
     String password = args[2];
     Class.forName("com.mysql.cj.jdbc.Driver");
-    java.sql.Connection conn = DriverManager.getConnection(
+    try {
+      java.sql.Connection conn = DriverManager.getConnection(
           "jdbc:mysql://" + host + ":4000/test?user=" + user + "&password=" + password + "&sslMode=VERIFY_IDENTITY&enabledTLSProtocols=TLSv1.2,TLSv1.3"
-    );
-    Statement stmt = conn.createStatement();
-    ResultSet rs = stmt.executeQuery("SELECT DATABASE();");
-    if (rs.next()) {
-      System.out.println("using db: " + rs.getString(1));
+      );
+      Statement stmt = conn.createStatement();
+      try {
+        ResultSet rs = stmt.executeQuery("SELECT DATABASE();");
+        if (rs.next()) {
+          System.out.println("using db: " + rs.getString(1));
+        }
+      } catch (Exception e) {
+          throw e
       }
+    } catch (Exception e) {
+        throw e
     }
+  }
 }
