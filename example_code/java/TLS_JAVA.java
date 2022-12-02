3081088142e82e7886a34c0f246be43580df2261
shiyuhang <1136742008@qq.com>
2022-12-02 21:19:36 +0800

    Add java

---
+++ example_code/java/TLS_JAVA.java
@@ -0,0 +1,29 @@
+import java.sql.*;
+
+public class TLS_JAVA {
+  public static void main(String args[]){
+    if(args.length !=3) {
+      throw new Error("Usage: java TLS_JAVA <host> <port> <database>");
+    }
+    String user = args[0];
+    String host = args[1];
+    String password = args[2];
+    Class.forName("com.mysql.cj.jdbc.Driver");
+    try {
+      java.sql.Connection conn = DriverManager.getConnection(
+          "jdbc:mysql://" + host + ":4000/test?user=" + user + "&password=" + password + "&sslMode=VERIFY_IDENTITY&enabledTLSProtocols=TLSv1.2,TLSv1.3"
+      );
+      Statement stmt = conn.createStatement();
+      try {
+        ResultSet rs = stmt.executeQuery("SELECT DATABASE();");
+        if (rs.next()) {
+          System.out.println("using db: " + rs.getString(1));
+        }
+      } catch (Exception e) {
+        System.out.println("exec error: " + e);
+      }
+    } catch (Exception e) {
+      System.out.println("connect error: " + e);
+    }
+  }
+}
