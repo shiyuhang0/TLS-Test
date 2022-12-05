8a43a0fea6a978e40f97a8bc148f4da2585f77d6
shiyuhang <1136742008@qq.com>
2022-12-05 13:58:37 +0800

    test java

--- example_code/java/Tls.java
+++ example_code/java/Tls.java
@@ -1,6 +1,6 @@
 import java.sql.*;
 
-public class TLS {
+public class Tls {
   public static void main(String args[]) throws Exception {
     if(args.length !=3) {
       throw new Error("Usage: java TLS_JAVA <host> <port> <database>");
