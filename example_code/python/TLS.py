4f574b0fe68e1d133f41dc8dde1b3e096ff51f71
shiyuhang <1136742008@qq.com>
2022-12-09 23:05:24 +0800

    optimize timezone

--- example_code/python/TLS.py
+++ example_code/python/TLS.py
@@ -3,15 +3,13 @@ import sys
 import platform
 
 kwargs = {
-  "host": sys.argv[1],
-  "port": 4000,
-  "user": sys.argv[2],
-  "password": sys.argv[3],
-  "database": "test",
-  "ssl": {
-    "ca": sys.argv[4]
-  }
 }
+kwargs["host"]=sys.argv[1]
+kwargs["port"]=4000
+kwargs["user"]=sys.argv[2]
+kwargs["password"]=sys.argv[3]
+kwargs["database"]="test"
+kwargs["ssl"]={"ca": sys.argv[4]}
 if platform.system() != 'Windows':
   kwargs['ssl_mode'] = "VERIFY_IDENTITY",
 connection = MySQLdb.connect(**kwargs)
